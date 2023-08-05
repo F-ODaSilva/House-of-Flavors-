0a7972086aad295d1676db4f172e6fdc2b8bf03d
F-ODaSilva <franloodasilva@gmail.com>
2023-08-05 11:26:14 +0200

    Moved form validation script to JS folder and did minor changes on the select box of the forms

--- Javascript/aboutPage.js
+++ Javascript/aboutPage.js
@@ -104,4 +104,21 @@ $(function(){
  });
 
 
+// JOIN US SECTION STARTS HERE 
+// Form validation script
+     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+     var forms = document.querySelectorAll('.needs-validation')
+
+     // Loop over them and prevent submission
+     Array.prototype.slice.call(forms).forEach(function (form) 
+     {
+             form.addEventListener('submit', function (event) {
+             if (!form.checkValidity()) {
+                 event.preventDefault();
+                 event.stopPropagation();
+             }
+
+             form.classList.add('was-validated');
+         }, false);
+     });
 
